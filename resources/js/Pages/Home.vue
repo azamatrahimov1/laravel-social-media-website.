e5b841e7c8db4081b72e7e14a84c4ad9e1bc6a06
azamatrahimov1 <azamatrahimov80@gmail.com>
2024-04-01 11:44:44 +0500

    Define website home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,15 +1,28 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+import {Head, Link} from '@inertiajs/vue3';
+import GroupList from "@/Components/app/GroupList.vue";
+import FollowingList from "@/Components/app/FollowingList.vue";
+import CreatePost from "@/Components/app/CreatePost.vue";
+import PostList from "@/Components/app/PostList.vue";
 
-defineProps({
-
-});
+defineProps({});
 </script>
 
 <template>
-    <Head title="Social Media Website" />
+    <Head title="Social Media Website"/>
 
-
+    <div class="grid grid-cols-12 gap-3">
+        <div class="col-span-3 py-6">
+            <GroupList />
+        </div>
+        <div class="col-span-6">
+            <CreatePost />
+            <PostList />
+        </div>
+        <div class="col-span-3 py-6">
+            <FollowingList />
+        </div>
+    </div>
 </template>
 
 <style>
